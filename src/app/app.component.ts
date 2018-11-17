8b7b5a1c06556751b204913c470a1dc583867a72
dario zubaray <dzubaray@gmail.com>
2018-11-17 15:26:30 -0300

    obteniendo los archivos y validarlos en la directiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  ngOnInit(): void {
+    window.addEventListener("dragover", e => e && e.preventDefault(), false);
+    window.addEventListener("drop", e => e && e.preventDefault(), false);
+  }
 }
--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -11,12 +11,57 @@ export class NgDropFilesDirective {
 
   @HostListener('dragover', ['$event'])
   public onDragEnter( event: any ) {
+    console.log("OnDragEnter");
+    this._prevenirDetener( event );
     this.mouseSobre.emit( true );
   }
 
   @HostListener('dragleave', ['$event'])
   public onDragLeave( event: any ) {
+    console.log("OnDragLeave");
     this.mouseSobre.emit( false );
+  }
+
+  @HostListener('drop', ['$event'])
+  public onDrop( event: any ) {
+    console.log("OnDrop");
+
+    const transferencia = this._getTransferencia( event );
+    if( !transferencia ) {
+      console.log("no se pudo obtener la transferencia");
+      return;
+    }
+
+    this._extraerArchivos( transferencia.files );
+    this._prevenirDetener( event );
+    this.mouseSobre.emit( false );
+  }
+
+  private _getTransferencia( event: any ) {
+    console.log(event);
+    if( event.dataTransfer ){
+      console.log("event.dataTransfer");
+      return event.dataTransfer;
+    } else if (event.originalEvent.dataTransfer ){
+      console.log("event.originalEvent.dataTransfer");
+      return event.originalEvent.dataTransfer;
+    } else {
+      console.log("null");
+      return null;
+    }
+  }
+
+  private _extraerArchivos( archivosLista: FileList ) {
+    // console.log(archivosLista);
+
+    for( const propiedad in Object.getOwnPropertyNames( archivosLista ) ) {
+      const archivoTemporal = archivosLista[propiedad];
+      if( this._archivoPuedeSerCargado( archivoTemporal ) ){
+        const nuevoArchivo = new FileItem( archivoTemporal );
+        this.archivos.push(nuevoArchivo);
+      }
+    }
+    console.log( this.archivos );
   }
 
   /* Validaciones */
@@ -34,7 +79,7 @@ export class NgDropFilesDirective {
   private _archivoYaFueDroppeado( nombreArchivo: string ): boolean {
     for(const archivo of this.archivos ){
       if(archivo.nombreArchivo == nombreArchivo ) {
-        console.log("El archivo " + nombreArchivo + " ya està agregado");
+        console.log("El archivo " + nombreArchivo + " ya está agregado");
         return true;
       }
     }
